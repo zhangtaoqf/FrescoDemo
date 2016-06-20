b37e68a2420b1f703eef423acfc15d88179e1a79
yyy <553873475@qq.com>
2016-06-20 17:48:22 +0800

    Initial commit

---
+++ app/src/main/java/zt/qf/com/frescodemo/MainActivity.java
@@ -0,0 +1,16 @@
+package zt.qf.com.frescodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 这个是我写的代码。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
